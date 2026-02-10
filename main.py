933d5cb56043acbac1bc1d370c69f0fd6b6cdd77
LuisLopesDEV <LuisLopesDEV1@gmail.com>
2026-02-10 15:32:03 -0300

    Add a HomePage from my API

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
 
 from Routes.auth import auth_router
 from Routes.users import users_router
