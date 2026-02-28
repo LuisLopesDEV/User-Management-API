b3ef0e61f5adf4c955ea49b8a403c4992c33b268
LuisLopesDEV <LuisLopesDEV1@gmail.com>
2026-02-28 09:33:32 -0300

    teste cors

--- Backend/main.py
+++ Backend/main.py
@@ -7,14 +7,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://auror-cafe-1.onrender.com",
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
