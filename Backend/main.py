d47dd909fadd5ba798c1fd1c90d9b5e20ad00e8d
LuisLopesDEV <LuisLopesDEV1@gmail.com>
2026-02-28 09:28:11 -0300

    Add my site to  CORSMiddleware.

--- Backend/main.py
+++ Backend/main.py
@@ -8,10 +8,11 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:3000",
-        "http://localhost:3000",
-        "https://user-management-api-coral.vercel.app",
-        "https://auror-cafe-1.onrender.com"
+        "https://auror-cafe-1.onrender.com",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
