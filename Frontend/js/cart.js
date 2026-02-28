19cb8b75bef7b8a63cb04b675066e8a574a9abfd
LuisLopesDEV <LuisLopesDEV1@gmail.com>
2026-02-28 09:05:05 -0300

    Change API_BASE

--- Frontend/js/cart.js
+++ Frontend/js/cart.js
@@ -1,4 +1,4 @@
-const API_BASE = window.API_BASE || "http://127.0.0.1:8000";
+const API_BASE = "http://127.0.0.1:8000";
 
 document.getElementById("cadastro").addEventListener("submit", async (e) => {
   e.preventDefault();
